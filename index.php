66fafb2afc4814df541efb60f98f569cc61d2006
Daly2025 <lydiafdez84@gmail.com>
2025-02-14 19:50:42 +0100

    cambios

--- index.php
+++ index.php
@@ -2,21 +2,23 @@
 require 'vendor/autoload.php';
 require 'database.php';
 
-use App\Models\Actor;  // Asegúrate de que la ruta y el nombre de la clase sean correctos
+use App\Models\Actor;
 use App\Models\Film;
 
-$peliculas = Film::all();  // Obtener todas las películas
 
-foreach ($peliculas as $film) {
-    echo $film->title;  // Mostrar el título de la película
-    
-    // Obtener todos los actores relacionados con la película
-    $actors = $film->actors()->get();  // Aquí usamos get() para obtener la colección de actores
 
-    // Iterar a través de los actores
-    foreach ($actors as $actor) {
-        echo $actor->first_name . "; ";  // Mostrar el nombre del actor
-    }
-    
-    echo "<br>";  // Nueva línea después de cada película
+$actor=Actor::find(5);
+echo $actor->first_name;
+echo "<br>";
+$pelicula=Film::find(3);
+foreach ($pelicula->actors as $key => $value) {
+    echo $value->first_name." , ";
 }
+echo "<br>";
+$pelicula->actors()->attach($actor->actor_id);
+$pelicula=Film::find(3);
+foreach ($pelicula->actors as $key => $value) {
+    echo $value->first_name." , ";
+}
+    
+  
